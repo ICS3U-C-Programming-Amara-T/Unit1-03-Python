064bed69f26a2dc0c24f18bf1d62eed4263cec00
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-14 04:35:53 +0000

    Changed the message

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created By: Amara Tie
+
+# Date: 12 Feb 2025
+
+# This program demonstrates how to write comments.
+
+
+def main():
+    print("Hello World with Style!")
+
+
+if __name__ == "__main__":
+    main()
